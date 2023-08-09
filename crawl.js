c09f6a560f123ab25109b40698907a1245d2b12a
Zedrichu <adrzvizdencojr@gmail.com>
2023-08-10 00:09:04 +0200

    Crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const response = await fetch(currentURL)
+        
+        if (response.status > 399) {
+            console.log(`Error on fetch with status code: ${response.status} on page ${currentURL}`)
+            return 
+        }
+
+        const contentType = response.headers.get("content-type")
+
+        if (!contentType.includes("text/html")) {
+            console.log(`Non-HTML response, content-type: ${contentType}, on page ${currentURL}`)
+        }
+
+        console.log(await response.text())
+    } catch(err) {
+        console.log(`Error in fetch: ${err.message} on page ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -35,5 +58,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello world!')
+const {crawlPage} = require('./crawl.js')
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("No website provided!")
+        process.exit(1)
+    }
+
+    if (process.argv.length > 3) {
+        console.log("Too many command-line arguments")
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`Starting crawl of ${baseURL}`)
+    // Argv: interpreter -> program entrypoint -> other args
+    crawlPage(baseURL)
+}
+
+main()
