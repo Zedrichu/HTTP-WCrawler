b10f402741612b69de734393d133e4d5ce320540
Zedrichu <adrzvizdencojr@gmail.com>
2023-08-09 18:10:14 +0200

    GetURLs function and associated tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,29 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    linkElements.forEach((item) => {
+        if (item.href.slice(0,1) === '/') {
+                try {
+                    const urlObj = new URL(`${baseURL}${item.href}`)
+                    urls.push(urlObj.href)
+                } catch(err) {
+                    console.log(`error with relative url: ${err.message}`)
+                }
+            } else { 
+                try {
+                    const urlObj = new URL(`${item.href}`)
+                    urls.push(urlObj.href)
+                } catch(err) {
+                    console.log(`error with relative url: ${err.message}`)
+                } 
+            }
+    })
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +34,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
